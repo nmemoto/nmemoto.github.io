95f885291b25e159d11a57d9677451a8648d0f7e
Takafumi Umemoto <takafumi.umemoto@gmail.com>
2021-02-06 17:59:12 +0000

    Add a setting to decorate markdown

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,19 @@
+import { MDXProvider } from "@mdx-js/react";
 import "../styles/globals.css";
 
+const components = {
+  h1: (props) => <h1 className="font-bold text-4xl" {...props} />,
+  h2: (props) => <h2 className="font-bold text-3xl" {...props} />,
+  h3: (props) => <h3 className="font-bold text-2xl" {...props} />,
+  h4: (props) => <h4 className="font-bold text-xl" {...props} />,
+};
+
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <MDXProvider components={components}>
+      <Component {...pageProps} />;
+    </MDXProvider>
+  );
 }
 
 export default MyApp;
