03f8cabcd66508beec1f2f487ff1a2f8b0b5362e
Takafumi Umemoto <takafumi.umemoto@gmail.com>
2021-02-22 00:40:59 +0900

    Update

--- utils/getFiles.ts
+++ utils/getFiles.ts
@@ -1,6 +1,18 @@
 import fs from "fs";
+import matter from "gray-matter";
 import path from "path";
 
 const root = process.cwd();
 
-export const getFiles = async () => fs.readdirSync(path.join(root, "contents"));
+export const getFiles = async () => {
+  const allFileNames = fs.readdirSync(path.join(root, "contents"));
+  const posts = allFileNames.filter((fileName) => {
+    const file = fs.readFileSync(
+      path.join(root, "contents", fileName),
+      "utf-8"
+    );
+    const { data }: { data: { published?: boolean } } = matter(file);
+    return data.published;
+  });
+  return posts;
+};
